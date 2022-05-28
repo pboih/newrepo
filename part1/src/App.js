b9b10fe19722c85eed7952aa17953d2da77f5c8d
attila <devanddesign@protonmail.com>
2022-05-29 02:58:10 +0300

    react-part1-courseinfo2

--- part1/src/App.js
+++ part1/src/App.js
@@ -14,13 +14,23 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
-       <p>
-      {props.part} {props.exercises}
+      <p>
+        {props.part} {props.exercises}
       </p>
     </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+       <Part part={part1} exercises={exercises1}/>
+       <Part part={part2} exercises={exercises2}/>
+       <Part part={part3} exercises={exercises3}/>
+    </div>
   )
 }
 
@@ -37,9 +47,7 @@ const App = () => {
   return (
     <div>
       <Header course = {course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content />
       <Total/>
     </div>
   )
