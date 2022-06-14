9a55d5cab84c77287efdb75cd115150cc0688015
attila <devanddesign@protonmail.com>
2022-06-14 15:19:13 +0300

    update structure of exercises

---
+++ part1-unicafe/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+
+const Button = ({handleClick, text}) => (
+<button onClick={handleClick}>{text}</button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text="good"/>
+      <Button handleClick={handleNeutral} text="neutral"/>
+      <Button handleClick={handleBad} text="bad"/>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
