ecc93b2e291dc8edf9a180fbb08f594e17e7cab1
attila <devanddesign@protonmail.com>
2022-06-21 12:54:11 +0300

    part 2 step 3 added phone number functionality

--- part2-phonebook/src/App.js
+++ part2-phonebook/src/App.js
@@ -4,15 +4,19 @@ import { useState } from 'react'
 
 const Numbers = (props) => {
   return (
-  <div>{props.name}</div>
+  <div>{props.name} {props.phone}</div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas', 
+      number: '040-1164487'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
   
   
 
@@ -20,6 +24,7 @@ const App = () => {
     event.preventDefault()
     const nameObject = {
       name: newName,
+      number: newPhone
     }
    const isFound = persons.some(element =>{
     if (element.name === nameObject.name){
@@ -32,10 +37,15 @@ const App = () => {
     : 
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewPhone('')
   }
  
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
   }
 
 
@@ -50,13 +60,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newPhone} onChange={handlePhoneChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => 
-          <Numbers key={person.name} name = {person.name}/>
+          <Numbers key={person.name} name={person.name} phone={person.number}/>
           )}
       </ul>
     </div>
