df90cb91b3ae5bf37361ec5081d842c34e51cf53
attila <devanddesign@protonmail.com>
2022-06-21 18:38:30 +0300

    part2 step4 TODO case insensitivity

--- part2-phonebook/src/App.js
+++ part2-phonebook/src/App.js
@@ -10,13 +10,14 @@ const Numbers = (props) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas', 
-      number: '040-1164487'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filter, setFilter] = useState('')
   
   
 
@@ -24,18 +25,19 @@ const App = () => {
     event.preventDefault()
     const nameObject = {
       name: newName,
-      number: newPhone
+      number: newPhone,
+      id: persons.length +1
     }
-   const isFound = persons.some(element =>{
-    if (element.name === nameObject.name){
+   const isFound = persons.some(element =>{ //if name is same as input return true
+    if (element.name.toUpperCase() === nameObject.name.toUpperCase()){ //compare names in uppercase (case insensitive)
       return true
     }
    })
-  
-   isFound
+   
+   isFound  //if that input already exists alarm user that it already exists
     ? alert(`${newName} is already added to the phonebook`)
     : 
-      setPersons(persons.concat(nameObject))
+      setPersons(persons.concat(nameObject)) //add new Object to array
       setNewName('')
       setNewPhone('')
   }
@@ -48,12 +50,25 @@ const App = () => {
     setNewPhone(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+  
+  const filterInput = filter.toUpperCase() // TODO filter case insensitive
 
+  const nameFilter = persons.map(person => person.name.includes(filter) ) //if each name includes input
+    ? persons.filter(x => x.name.includes(filter)) //Array becomes filtered Names
+    : persons //Array remains full
 
+  
   return (
     
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={filter} onChange={handleFilterChange}/></div>
+
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
     
         <div>
@@ -68,7 +83,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
+        {nameFilter.map(person => 
           <Numbers key={person.name} name={person.name} phone={person.number}/>
           )}
       </ul>
