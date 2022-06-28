ccae8cee9c2b999cfd2342775b0ddc7e1b02881a
attila <devanddesign@protonmail.com>
2022-06-28 23:02:09 +0200

    countries weather functionality added

--- part2-countries/src/App.js
+++ part2-countries/src/App.js
@@ -20,9 +20,22 @@ const Countries = (props) => {
     <p>Too many matches, specify another filter. </p>
   
   )
-  else if(props.countries.length === 1) return (
-    props.countries.map(x =>
-      <>
+  else if(props.countries.length === 1){
+  let weatherString
+    if(props.weather.length != 0 ){
+      weatherString = (<>
+      <h2>Weather in {props.countries[0].capital}</h2>
+      <p>Temperature {props.weather.main.temp} Celcius</p>
+      <img src= {(`http://openweathermap.org/img/wn/${props.weather.weather[0].icon}@2x.png`)}/>
+      <p>wind {props.weather.wind.speed} m/s</p>
+
+      </>)
+    }
+  
+  return (
+    <>
+    {props.countries.map(x =>
+      <div key={x.name.common}>
       <h1>{x.name.common}</h1>
       
       <p>capital {x.capital}</p>
@@ -31,15 +44,19 @@ const Countries = (props) => {
       <h3>languages:</h3>
       <ul>
         {Object.values(x.languages).map( y => 
-        <li>
+        <li key={y}>
           {y}
         </li>)}
       </ul>
 
       <img src={x.flags.png}/>
+
+      {weatherString}
+      </div>
+      )}
+      
       </>
-      )
-  )
+  )}
   else return (<ul>
   {
      props.countries.map(x =>
@@ -62,36 +79,54 @@ const Filter = (props) => {
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('') 
-  
-  
+  const [weather, setWeather] = useState([])
+  const [listedCountries, setListedCountries] = useState([])
+  const api_key = "2598af7a20b25c268ba0ffb1d45d1404"
+  //Countries
   useEffect(() => {
-    console.log('effect')
+    
     axios
     .get('https://restcountries.com/v3.1/all')
     .then(response => {
-      console.log('promise fulfilled')
+      
       setCountries(response.data)
-      console.log(response.data)
+      
     })
   }, [])
-  console.log('render', countries.length, 'countries')
-  console.log(countries)
+ 
+  const filterInput = filter.toUpperCase()
+  const countriesFilter = countries.filter(x => x.name.common.toUpperCase().includes(filterInput) )
   
-
+  //Weather
+  useEffect(() => {
+  if(countriesFilter.length === 1){
+    
+    let lat = countriesFilter[0].latlng[0]
+    let lon = countriesFilter[0].latlng[1]
+    axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`)
+    .then(response => {
+      setWeather(response.data)
+      console.log(response.data)
+      /* props.setWeather(response.data) */
+    })
+  }}, [filter])
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
   }
 
-  const filterInput = filter.toUpperCase()
-  const countriesFilter = countries.filter(x => x.name.common.toUpperCase().includes(filterInput) )
- 
+
+
 
     return (
     
     <div>
       <Filter filterValue={filter} filterHandler={handleFilterChange}/>
-      <Countries countries={countriesFilter} input={filterInput} setFilter={setFilter}/>
+      <Countries countries={countriesFilter} 
+                 input={filterInput} 
+                 setFilter={setFilter}
+                 weather={weather}/>
     </div>
   )
 }
